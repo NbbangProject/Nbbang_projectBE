af67a83096890228d2137c181bb7a3345944ebae
pieceofcakey <choi_927@naver.com>
2022-06-13 21:30:16 +0900

    commentAll 추가

--- schemas/posts.js
+++ schemas/posts.js
@@ -45,6 +45,11 @@ const postsSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  commentAll: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model('Posts', postsSchema);
