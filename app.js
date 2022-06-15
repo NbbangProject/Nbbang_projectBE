20d9386535c672a58896f499605ec3a522f81720
pieceofcakey <choi_927@naver.com>
2022-06-15 16:24:10 +0900

    app.use 추가

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use('/api', require('./routes/comment.js'));
 app.use('/api', require('./routes/post.js'));
 app.use('/api', require('./routes/user.js'));
+app.use(express.static('uploads'));
 
 app.use(cors());
 app.use((req, res, next) => {
