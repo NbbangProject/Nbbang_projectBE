6c38d91290433c571b593d90d68e615cee9a7824
pieceofcakey <choi_927@naver.com>
2022-06-16 00:56:26 +0900

    cors 위치변경

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 const connect = require('./schemas');
-
+app.use(cors());
 connect();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -12,7 +12,6 @@ app.use('/api', require('./routes/post.js'));
 app.use('/api', require('./routes/user.js'));
 app.use(express.static('uploads'));
 
-app.use(cors());
 app.use((req, res, next) => {
   console.log('Request URL:', req.originalUrl, ' - ', new Date());
   next();
